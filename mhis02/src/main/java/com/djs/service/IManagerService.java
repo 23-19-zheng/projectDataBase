cb6f4d4b6cce06f99fac5a364bbaee8824bc1a34
23-19-zheng <82429325+23-19-zheng@users.noreply.github.com>
2021-05-16 23:18:38 +0800

    Delete mhis02 directory

--- mhis02/src/main/java/com/djs/service/IManagerService.java
+++
@@ -1,9 +0,0 @@
-package com.djs.service;
-
-import java.util.List;
-
-import com.djs.entity.Manager;
-
-public interface IManagerService {
-	List<Manager> findAllByIdAndPassword(Integer number,String password);
-}
